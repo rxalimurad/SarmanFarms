e717ece317efa60809114d83f65d161f2455666e
Ali Murad <40003230+rxalimurad@users.noreply.github.com>
2023-06-26 22:32:54 +0500

    Initial Commit

---
+++ SarmanFarms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SarmanFarms
+//
+//  Created by Ali Murad on 26/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
